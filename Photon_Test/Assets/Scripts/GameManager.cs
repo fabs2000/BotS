c5bcd1bf53802602178485e1fbc2c1183618e02a
fabs2000 <fabiospinelli2000@gmail.com>
2021-04-13 17:36:49 +0200

    Final push of the day, almost done with Photon Tutorial

--- Photon_Test/Assets/Scripts/GameManager.cs
+++ Photon_Test/Assets/Scripts/GameManager.cs
@@ -23,9 +23,36 @@ namespace Com.MyCompany.MyGame
             SceneManager.LoadScene(0);
         }
 
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", newPlayer.NickName); // not seen if you're the player connecting
+
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+
+                LoadArena();
+            }
+        }
+        
+        public override void OnPlayerLeftRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+
+                LoadArena();
+            }
+        }
+
         #endregion
-
-
+        
         #region Public Methods
 
         public void LeaveRoom()
@@ -35,5 +62,18 @@ namespace Com.MyCompany.MyGame
 
         #endregion
 
+        #region Private Methods
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+        }
+        
+        #endregion
     }
 }
